e144c2ec9d464f05a143e2abd911bfeb58f755ae
heidichen90 <toddypet@gmail.com>
2021-10-18 23:34:29 +0100

    run app.js with javscript standard

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
-const routes = require("./routes");
-require("./config/mongoose");
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+require('./config/mongoose')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-//express template engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// express template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-//setup static files
-app.use(express.static("public"));
+// setup static files
+app.use(express.static('public'))
 
-//setup body-parser
-app.use(express.urlencoded({ extended: true }));
+// setup body-parser
+app.use(express.urlencoded({ extended: true }))
 
-//set up method override
+// set up method override
 // override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'))
 
-//set up router
-app.use(routes);
+// set up router
+app.use(routes)
 
-//start and listen on the Express server
+// start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`);
-});
+  console.log(`Express is listening on localhost:${port}`)
+})
