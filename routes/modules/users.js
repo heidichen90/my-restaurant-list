5f85817c24c2154c3d8ec8e2e48027b50acd9765
heidichen90 <toddypet@gmail.com>
2021-10-19 22:44:14 +0100

    add check on register form

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+
 const Restaurant = require("../../models/restaurants");
+const User = require("../../models/users");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -16,7 +19,53 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  res.send("register");
+  const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
+  //check register form field
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: "All fields required." });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: "Password didnt match Confirm Password." });
+  }
+
+  //check if user exist
+  if (errors.length) {
+    return res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  } else {
+    //check if user exist
+    console.log("check if user exists");
+    User.findOne({ email })
+      .then((user) => {
+        if (user) {
+          errors.push({ message: "This email is registed" });
+          return res.render("register", {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword,
+          });
+        } else {
+          //if user dont exist, then create user
+          return bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(password, salt))
+            .then((hash) => User.create({ name, email, password: hash }))
+            .then(() => {
+              res.redirect("/");
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 router.get("/logout", (req, res) => {
